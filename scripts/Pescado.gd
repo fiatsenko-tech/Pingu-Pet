80d799079cbe3c7fe01d059cf66d7c4676b6707e
fiatsenko-tech <felixiatsenko@gmail.com>
2026-09-09 22:51:36 -0300

    Comer pescado terminado

--- scripts/Pescado.gd
+++ scripts/Pescado.gd
@@ -14,30 +14,29 @@ func _gui_input(event):
 				arrastrando = false
 				comprobar_entrega()
 
+
 func _process(_delta):
 	if arrastrando:
 		rect_global_position = get_global_mouse_position() + offset
+
 
 func comprobar_entrega():
 	var pingu = get_tree().get_root().get_node("Juego/Pingu")
 	var zona_comida = pingu.get_node("ZonaComida")
 
 	var posicion_pescado = get_global_rect().get_center()
-	var posicion_zona = zona_comida.global_position
+	var posicion_pescado_local = pingu.to_local(posicion_pescado)
 
-	print("Pescado: ", posicion_pescado)
-	print("Zona comida: ", posicion_zona)
+	var distancia = posicion_pescado_local.distance_to(zona_comida.position)
 
-	var distancia = posicion_pescado.distance_to(posicion_zona)
+	print("Distancia del pescado a ZonaComida: ", distancia)
 
-	print("Distancia: ", distancia)
-
-	if distancia < 300:
+	if distancia <= 42.72:
 		if Necesidades.hambre < 100:
-			print("¡Pingu puede comer!")
 			Necesidades.modificar_hambre(17)
-			print("Hambre después de comer: ", Necesidades.hambre)
+			print("¡Pingu comió!")
+			print("Hambre: ", Necesidades.hambre)
 		else:
 			print("Pingu está lleno")
 	else:
-		print("Pescado soltado fuera de Pingu")
+		print("Pescado soltado fuera de ZonaComida")
