1c3b4888ada2f30bd7e495cfeeef1fa9aa6d8eae
fiatsenko-tech <felixiatsenko@gmail.com>
2026-09-12 21:28:47 -0300

    Comedor en proceso

--- scripts/Refrigerador.gd
+++ scripts/Refrigerador.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 var abierto = false
+var cantidad_pescado_plato = 0
 
 onready var zona_comida = $ZonaComida
 onready var panel = $PanelRefrigerador
 onready var boton_pescado = $PanelRefrigerador/EspacioPescado/TextureButton
 onready var label_pescado = $PanelRefrigerador/EspacioPescado/Label
 onready var boton_cerrar = $PanelRefrigerador/BotonCerrar
-
+onready var pescado_plato = get_node("../Plato/TextureButtonAlimento")
+onready var label_plato = get_node("../Plato/Label")
 
 func _ready():
 	zona_comida.connect("input_event", self, "_on_ZonaComida_input_event")
@@ -15,7 +17,8 @@ func _ready():
 	boton_pescado.connect("pressed", self, "_on_BotonPescado_pressed")
 	
 	panel.hide()
-	
+	pescado_plato.hide()
+	label_plato.hide()
 
 
 func _on_ZonaComida_input_event(viewport, event, shape_idx):
@@ -51,10 +54,21 @@ func _on_BotonPescado_pressed():
 		return
 	
 	Inventario.food["pescado"] -= 1
+	cantidad_pescado_plato += 1
 	
 	actualizar_inventario()
+	actualizar_plato()
 	
 	print("Pescado retirado. Quedan: ", Inventario.food["pescado"])
+
+func actualizar_plato():
+	if cantidad_pescado_plato > 0:
+		pescado_plato.show()
+		label_plato.show()
+		label_plato.text = "x " + str(cantidad_pescado_plato)
+	else:
+		pescado_plato.hide()
+		label_plato.hide()
 
 
 func _on_BotonCerrar_pressed():
